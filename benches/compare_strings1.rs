6db43cc84f4e65ae7c0fe484a58f836828657db7
Russ <rmorris@infocyte.com>
2018-01-22 10:04:45 -0800

    0.1.4 (#7)
    * add string comparison benchmark
    * ~26% faster hash comparisons, updated tests, added some docs
    * roll to version 0.1.4

---
+++ benches/compare_strings1.rs
@@ -0,0 +1,19 @@
+#![feature(test)]
+extern crate fuzzyhash;
+extern crate test;
+
+use fuzzyhash::compare::{strs};
+use test::Bencher;
+
+#[bench]
+fn compare_bench(b: &mut Bencher) {
+    let string1 = "96:U57GjXnLt9co6pZwvLhJluvrszNgMFwO6MFG8SvkpjTWf:Hj3BeoEcNJ0TspgIG8SvkpjTg".to_string();
+    let string2 = "96:U57GjXnLt9co6pZwvLhJluvrs1eRTxYARdEallia:Hj3BeoEcNJ0TsI9xYeia3R".to_string();
+    
+    b.iter(|| {
+        strs(
+            &string1,
+            &string2
+        );
+    });
+}
